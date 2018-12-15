758db648908118fd3504ba19b28b08640b61fd59
Giorgio Lucca <giorgio.lucca@praxis.com.br>
2018-12-15 11:46:42 -0200

    Create paginated response

---
+++ src/PaginatedResponse.php
@@ -0,0 +1,89 @@
+<?php
+
+class PaginatedResponse
+{
+	private $data;
+	private $metadata;
+
+	/**
+	 * @param array $data
+	 * @param \stdClass $metadata
+	 */
+	public function __construct(array $data, \stdClass $metadata)
+	{
+		$this->data = $data;
+		$this->metadata = $metadata;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getData()
+	{
+		return $this->data;
+	}
+
+	/**
+	 * @return \stdClass
+	 */
+	public function getMetadata()
+	{
+		return $this->metadata;
+	}
+
+	/**
+	 * @return integer
+	 */
+	public function getTotal()
+	{
+		return $this->metadata->pagination->total;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getCount()
+	{
+		return $this->metadata->pagination->count;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getPerPage()
+	{
+		return $this->metadata->pagination->per_page;	
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getCurrentPage()
+	{
+		return $this->metadata->pagination->current_page;	
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getTotalPages()
+	{
+		return $this->metadata->pagination->total_pages;	
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getNext()
+	{
+		return urldecode($this->metadata->pagination->links->next);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPrevious()
+	{
+		return urldecode($this->metadata->pagination->links->previous);	
+	}
+}
